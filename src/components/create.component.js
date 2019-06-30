058b8c80cec210c6948604e740f80c2e70d77a6b
assylkhanRakhatov <assylkhan.rakhatov@mail.ru>
2019-06-30 19:50:46 +0600

    validation

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -7,7 +7,7 @@ export default class Create extends Component {
     render() {
         return (
             <div style={{marginTop: 10}}>
-                <h3>Issue report validation</h3>
+                <h3 align=center>Issue report validation</h3>
                 <Form />
             </div>
         )
